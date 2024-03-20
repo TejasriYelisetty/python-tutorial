d491967932b83d8783ea51d108390a0d81b9b4c0
TejasriYelisetty <136186099+TejasriYelisetty@users.noreply.github.com>
2024-03-20 16:15:51 +0530

    01.Print statement .py

---
+++ 01.Print statement .py
@@ -0,0 +1,17 @@
+This is a Hello World program
+print("Hello World")
+
+/*
+Hello World
+*/
+
+print the statement in line by line
+
+print("Hello \n")
+print("Alexandra Abramov")
+
+/*
+Hello
+
+Alexandra Abramov
+*/
