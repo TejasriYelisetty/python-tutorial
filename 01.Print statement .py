1812ce402caed8793629f2e5ac8621d1cd7017f0
TejasriYelisetty <136186099+TejasriYelisetty@users.noreply.github.com>
2024-03-27 14:33:28 +0530

    01.Print statement .py

--- 01.Print statement .py
+++ 01.Print statement .py
@@ -15,3 +15,16 @@ Hello
 
 Alexandra Abramov
 */
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are!\n\t\t
+Up above the world so high,\n\t\tLike a diamond in the sky.\n 
+Twinkle, twinkle, little star,\n\tHow I wonder what you are")
+/*
+Twinkle, twinkle, little star, 
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+ Twinkle, twinkle, little star,
+	How I wonder what you are
+
+=== Code Execution Successful ===
+*/
